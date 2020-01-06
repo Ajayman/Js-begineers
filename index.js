9ccda8dc396f4245c315edd8de7959681e5468c6
Ajay Awal <ajayawal565@gmail.com>
2020-01-06 21:03:06 +0545

    variable rules

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 let name = "Charan";
 name = "Saran";
 //here name value can be assigned with different value.
+//Cannot be a reserved keyword
+//Cannot start with a number (1name)
+//Cannot contain a space or hypen(-)
+// Are case-sensitive
 
+
+const interestRate = 10;
+interestRate = 20;
+//interestRate is a constant variable or fixed
 
