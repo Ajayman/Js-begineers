4f39acc18a5b45f2be0d048bb8229bedb4c3cf1b
Ajay Awal <ajayawal565@gmail.com>
2020-01-06 23:03:30 +0545

    person object create

--- index.js
+++ index.js
@@ -8,6 +8,29 @@ name = "Saran";
 
 
 const interestRate = 10;
-interestRate = 20;
+//interestRate = 20;
 //interestRate is a constant variable or fixed
 
+//Two types of values we assign to variables(Primitive/Value Types and Reference Type)
+let name = 'Mosh';  // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let selectedColor = null;
+
+//Reference Types Object, Array, Function
+
+// Object means part of the representaion of
+
+let person = {
+    name : 'Mosh',
+    age : 30
+};
+
+// Dot Notation
+person.name = 'John';
+
+//Bracket Notation
+person['name'] = 'Mary';
+
+console.log(person.name)
